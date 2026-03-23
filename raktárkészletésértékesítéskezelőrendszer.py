4e89f01c773b3e6bb3a34c338179384afc961b3a
KNorbi69 <kakucsi.norbert.2024i@bankitatabanya.hu>
2026-03-23 09:26:48 +0100

    kereses

--- raktárkészletésértékesítéskezelőrendszer.py
+++ raktárkészletésértékesítéskezelőrendszer.py
@@ -5,8 +5,11 @@ class Termek:
         self.nev = nev
         self.ar = ar
         self.keszletmennyiseg = keszletmennyiseg
-<<<<<<< HEAD
-    
+    def adatok(self):
+        print(f"Név                  Ár(Ft)              Mennyiség (db)\n---------------------------------------------------------\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}            {self.keszészletmennyiseg}\n----------------------------------------------------------\nÖsszesen: {} féle termék a raktáron.")
+        
+termek = input("Melyik terméket szeretnéd megvásárolni? ")
+mennyiseg = input("")
 
 def eladas():
     termek = input("Melyik terméket szeretnéd megvásárolni? ")
@@ -21,10 +24,14 @@ def eladas():
         print(f"Eladva: {mennyiseg} db {termek}. Fizetendő: {osszeg}\n(Új készlet: {ujkeszlet} db)")   
 
 
-=======
-    def adatok(self):
-        print(f"Név                  Ár(Ft)              Mennyiség (db)\n---------------------------------------------------------\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}            {self.keszészletmennyiseg}\n----------------------------------------------------------\nÖsszesen: {} féle termék a raktáron.")
-        
-termek = input("Melyik terméket szeretnéd megvásárolni? ")
-mennyiseg = input("")
->>>>>>> 0b2f5e9638d54bce7f2d5d7c7e1e1cfeef5dea9d
+
+def kereses():
+    keres = input("Keresett kifejezés: ")
+    if keres not in termekek:
+        print("Nincs ilyen termék.")
+    else:
+        for ter in termekek:
+            if keres in ter:
+                print(f"[TALÁLAT] {ter.nev} - Ár: {ter.ar} Ft, Készlet: {ter.keszletmennyiseg} db")
+
+            
