4b6d533995d9b9ec95ba1edc236aa5f9ded7ff8e
KNorbi69 <kakucsi.norbert.2024i@bankitatabanya.hu>
2026-03-23 09:10:53 +0100

    eladas

--- raktárkészletésértékesítéskezelőrendszer.py
+++ raktárkészletésértékesítéskezelőrendszer.py
@@ -8,6 +8,16 @@ class Termek:
         self.keszletmennyiseg = keszletmennyiseg
     
 
+def eladas():
+    termek = input("Melyik terméket szeretnéd megvásárolni? ")
+    while termek not in termekek:
+        termek = input("Melyik terméket szeretnéd megvásárolni? ")
+    mennyiseg = input("Hány darabot szeretnél? ")
+    while not mennyiseg.isdigit() or int(mennyiseg) < 1:
+        mennyiseg = input("Nem megfelelő érték! Add meg újra: ")
+    if mennyiseg < termek.keszletmennyiseg:
+        ujkeszlet = termek.keszletmennyiseg - mennyiseg
+        osszeg = termek.ar * mennyiseg
+        print(f"Eladva: {mennyiseg} db {termek}. Fizetendő: {osszeg}\n(Új készlet: {ujkeszlet} db)")   
 
-termek = input("Melyik terméket szeretnéd megvásárolni? ")
-mennyiseg = input()
+
