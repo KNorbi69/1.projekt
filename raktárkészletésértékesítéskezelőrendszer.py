87bc210a455af378bef878ba52d6d6d6dd6abf38
LokarMilan <lokar.milan.albert.2024i@bankitatabanya.hu>
2026-03-23 08:47:19 +0100

    kaki

--- raktárkészletésértékesítéskezelőrendszer.py
+++ raktárkészletésértékesítéskezelőrendszer.py
@@ -1,2 +1,9 @@
+
 f = open("raktar.txt", "a", encoding="utf-8")
 f.readlines()
+class Termek:
+    def __init__(self,nev,ar,keszletmennyiseg):
+        self.nev = nev
+        self.ar = ar
+        self.keszletmennyiseg = keszletmennyiseg
+        
