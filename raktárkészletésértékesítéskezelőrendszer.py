0b2f5e9638d54bce7f2d5d7c7e1e1cfeef5dea9d
LokarMilan <lokar.milan.albert.2024i@bankitatabanya.hu>
2026-03-23 09:07:01 +0100

    tablazat

--- raktárkészletésértékesítéskezelőrendszer.py
+++ raktárkészletésértékesítéskezelőrendszer.py
@@ -1,13 +1,12 @@
 
 f = open("raktar.txt", "a", encoding="utf-8")
-f.readlines()
 class Termek:
     def __init__(self,nev,ar,keszletmennyiseg):
         self.nev = nev
         self.ar = ar
         self.keszletmennyiseg = keszletmennyiseg
-    
-
-
+    def adatok(self):
+        print(f"Név                  Ár(Ft)              Mennyiség (db)\n---------------------------------------------------------\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}           {self.keszletmennyiseg}\n{self.nev}             {self.ar}            {self.keszészletmennyiseg}\n----------------------------------------------------------\nÖsszesen: {} féle termék a raktáron.")
+        
 termek = input("Melyik terméket szeretnéd megvásárolni? ")
-mennyiseg = input()
+mennyiseg = input("")
